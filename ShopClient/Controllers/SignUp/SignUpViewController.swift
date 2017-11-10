2ad359e98014aec88cfc69d028346522ff15289c
Evgeniy Antonov <antonoff.evgeniy@gmail.com>
2017-11-10 18:01:56 +0200

    Added sign up form validation

--- ShopClient/Controllers/SignUp/SignUpViewController.swift
+++ ShopClient/Controllers/SignUp/SignUpViewController.swift
@@ -6,14 +6,49 @@
 //  Copyright © 2017 Evgeniy Antonov. All rights reserved.
 //
 
-import UIKit
+import RxSwift
 
 class SignUpViewController: BaseViewController<SignUpViewModel> {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         viewModel = SignUpViewModel()
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupViewModel()
+    }
+    
+    private func setupViewModel() {
+        emailTextField.rx.text.map({ $0 ?? String() })
+            .bind(to: viewModel.emailText)
+            .disposed(by: disposeBag)
+        
+        firstNameTextField.rx.text.map({ $0 ?? String() })
+            .bind(to: viewModel.firstNameText)
+            .disposed(by: disposeBag)
+        
+        lastNameTextField.rx.text.map({ $0 ?? String() })
+            .bind(to: viewModel.lastNameText)
+            .disposed(by: disposeBag)
+        
+        passwordTextField.rx.text.map({ $0 ?? String() })
+            .bind(to: viewModel.passwordText)
+            .disposed(by: disposeBag)
+        
+        phoneTextField.rx.text.map({ $0 ?? String() })
+            .bind(to: viewModel.phoneText)
+            .disposed(by: disposeBag)
+        
+        viewModel.isValid
+            .subscribe(onNext: { [weak self] isValid in
+                self?.signUpButton.isEnabled = isValid
+            })
+            .disposed(by: disposeBag)
     }
 }
--- ShopClient/Controllers/SignUp/SignUpViewModel.swift
+++ ShopClient/Controllers/SignUp/SignUpViewModel.swift
@@ -6,8 +6,28 @@
 //  Copyright © 2017 Evgeniy Antonov. All rights reserved.
 //
 
-import UIKit
+import RxSwift
+
+let kPasswordCharactersCountMin = 6
 
 class SignUpViewModel: BaseViewModel {
+    var emailText = Variable<String>(String())
+    var firstNameText = Variable<String>(String())
+    var lastNameText = Variable<String>(String())
+    var passwordText = Variable<String>(String())
+    var phoneText = Variable<String>(String())
+    
+    var isValid: Observable<Bool> {
+        return Observable.combineLatest(emailText.asObservable(), passwordText.asObservable()) { email, password in
+            email.isValidAsEmais() && password.characters.count >= kPasswordCharactersCountMin
+        }
+    }
+}
 
+internal extension String {
+    func isValidAsEmais() -> Bool {
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: self)
+    }
 }
