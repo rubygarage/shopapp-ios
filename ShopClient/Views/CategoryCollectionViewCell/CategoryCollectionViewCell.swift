2fa7d3cd43b9b75a7b671e55d35bf67bad77198e
Evgeniy Antonov <antonoff.evgeniy@gmail.com>
2017-12-20 16:10:30 +0200

    Removed empty method

--- ShopClient/Views/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
+++ ShopClient/Views/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -17,11 +17,6 @@ class CategoryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var categoryTitleLabel: UILabel!
     @IBOutlet weak var categoryImageView: UIImageView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-    
     func configure(with category: Category) {
         categoryTitleLabel.text = category.title
         let imageUrl = URL(string: category.image?.src ?? String())
