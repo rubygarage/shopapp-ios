f216f1ce1fb6f41e6da03b00ce3bd689fa64447d
Radyslav Krechet <radyslavkrechet@gmail.com>
2018-01-04 18:31:10 +0200

    Add order details use case and view model

--- ShopClient/App/Account/Controllers/OrderDetailsViewController.swift
+++ ShopClient/App/Account/Controllers/OrderDetailsViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class OrderDetailsViewController: BaseViewController<OrderDetailsViewModel> {
+    @IBOutlet weak var tableView: UITableView!
+    
     var orderId: String!
     
     // MARK: - view controller lifecycle
@@ -17,10 +19,44 @@ class OrderDetailsViewController: BaseViewController<OrderDetailsViewModel> {
         super.viewDidLoad()
         
         setupViews()
+        setupTableView()
+        setupViewModel()
+        loadData()
     }
     
     // MARK: - setup
     private func setupViews() {
         title = NSLocalizedString("ControllerTitle.OrderDetails", comment: String())
+    }
+    
+    private func setupTableView() {
+        /*
+        let cartCellNib = UINib(nibName: String(describing: CartTableViewCell.self), bundle: nil)
+        tableView.register(cartCellNib, forCellReuseIdentifier: String(describing: CartTableViewCell.self))
+        
+        tableDataSource = CartTableDataSource(delegate: self)
+        tableView.dataSource = tableDataSource
+        
+        tableDelegate = CartTableDelegate(delegate: self)
+        tableView.delegate = tableDelegate
+ */
+    }
+    
+    private func setupViewModel() {
+        viewModel.orderId = orderId
+        
+        errorView.tryAgainButton.rx.tap
+            .bind(to: viewModel.loadData)
+            .disposed(by: disposeBag)
+        
+        viewModel.data.asObservable()
+            .subscribe(onNext: { [weak self] products in
+                self?.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func loadData() {
+        viewModel.loadOrder()
     }
 }
