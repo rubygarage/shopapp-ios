c491cf1bd29dd9c1866725281aa1668d1ea66a1c
Evgeniy Antonov <antonoff.evgeniy@gmail.com>
2018-03-23 16:48:48 +0200

    Add tests, move modules folders to separated folder (#142)

---
+++ ShopAppTests/App/Extensions/Mocks/TTTAttributedLabelDelegateMock.swift
@@ -0,0 +1,11 @@
+//
+//  TTTAttributedLabelDelegateMock.swift
+//  ShopAppTests
+//
+//  Created by Evgeniy Antonov on 3/22/18.
+//  Copyright © 2018 RubyGarage. All rights reserved.
+//
+
+import TTTAttributedLabel
+
+class TTTAttributedLabelDelegateMock: NSObject, TTTAttributedLabelDelegate {}
